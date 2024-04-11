52a73ba85941885fa8cc9ca16d74eeb7c4e10d3b
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-04-11 11:24:12 +0200

    CI script

---
+++ test/hello.hh.js
@@ -0,0 +1,15 @@
+import * as hh from "@hop/hiphop";
+import * as config from "../dist/config.js";
+import mqtt from "mqtt";
+
+describe('HHMQTT', function () {
+  describe('hello()', function () {
+    it('mqtt connection', async  () => {
+       const cfg = config.init();
+       const client = mqtt.connect(cfg.server);
+       return new Promise((res, rej) => {
+	  client.on("connect", () => res(true));
+       });
+    });
+  });
+});
